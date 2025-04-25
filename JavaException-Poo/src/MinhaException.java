d34e948101808fe99df92e024eaabcbf30820124
Victor Ponciano <victordias.ponciano@gmail.com>
2025-04-25 18:24:21 -0300

    Uso do tratamento de exceções junto de POO

--- JavaException-Poo/src/MinhaException.java
+++ JavaException-Poo/src/MinhaException.java
@@ -2,10 +2,7 @@
 
 public class MinhaException extends Exception {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; //SERIAL VERSION - AINDA NAO SEI O PROPOSITO
 
 	public MinhaException(String mensagem) {
 		super(mensagem);
